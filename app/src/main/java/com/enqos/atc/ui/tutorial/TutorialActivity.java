5216a684ca76e4b3f169e5446c2ded866122ae2e
vinothlenin <vinothl@enqos.com>
2019-04-04 22:21:20 +0530

    Replaced the tutorial screen

--- app/src/main/java/com/enqos/atc/ui/tutorial/TutorialActivity.java
+++ app/src/main/java/com/enqos/atc/ui/tutorial/TutorialActivity.java
@@ -1,16 +1,15 @@
 package com.enqos.atc.ui.tutorial;
 
-import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.widget.TextView;
 
 import com.enqos.atc.R;
 import com.enqos.atc.base.AtcApplication;
 import com.enqos.atc.base.BaseActivity;
 import com.enqos.atc.ui.home.HomeActivity;
-import com.enqos.atc.ui.launcher.SplashActivity;
 import com.enqos.atc.utils.Constants;
 
 import java.util.ArrayList;
@@ -21,14 +20,15 @@ import javax.inject.Inject;
 import butterknife.BindView;
 import butterknife.OnClick;
 
-public class TutorialActivity extends BaseActivity implements TutorialView {
+public class TutorialActivity extends BaseActivity implements TutorialView, ViewPager.OnPageChangeListener {
     @Inject
     TutorialPresenter presenter;
     @BindView(R.id.view_pager)
     ViewPager viewPager;
     @BindView(R.id.tab_layout)
     TabLayout tabLayout;
-
+    @BindView(R.id.skip)
+    TextView tvSkip;
     @SuppressWarnings("unchecked")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +44,8 @@ public class TutorialActivity extends BaseActivity implements TutorialView {
         TutorialAdapter tutorialAdapter = new TutorialAdapter(this, resIds);
         viewPager.setAdapter(tutorialAdapter);
         tabLayout.setupWithViewPager(viewPager, true);
+
+        viewPager.addOnPageChangeListener(this);
     }
 
     @OnClick(R.id.skip)
@@ -63,5 +65,25 @@ public class TutorialActivity extends BaseActivity implements TutorialView {
     @Override
     public int getLayoutId() {
         return R.layout.activity_tutorial;
+    }
+
+    @Override
+    public void onPageScrolled(int i, float v, int i1) {
+
+    }
+
+    @Override
+    public void onPageSelected(int i) {
+
+        if(i == 4){
+            tvSkip.setText(R.string.done);
+        }else{
+            tvSkip.setText(getString(R.string.skip));
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int i) {
+
     }
 }
